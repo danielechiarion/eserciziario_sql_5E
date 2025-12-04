68babb1b38e5098f94359f60e5b466a1061bf054
unknown <ciaobello@iisviolamarchesini.edu.it>
2025-12-04 11:16:51 +0100

    completed second queries for the canteen database

--- Chiarion_5E_Es2A_mensa.sql
+++ Chiarion_5E_Es2A_mensa.sql
@@ -3,5 +3,6 @@ of a specific day */
 SELECT COUNT(piatto_menu_ordine.ordine) AS ordini_vegetariani FROM piatto_menu_ordine JOIN ordine ON piatto_menu_ordine.ordine = ordine.ID WHERE menu != "vegetariano" AND ordine.giorno = "2023-03-12";
 /* select the dish most most served */
 SELECT piatto,COUNT(ordine) AS conteggio FROM piatto_menu_ordine GROUP BY piatto ORDER BY conteggio DESC LIMIT 1;
+SELECT piatto FROM piatto_menu_ordine WHERE piatto = (SELECT piatto FROM piatto_menu_ordine GROUP BY piatto ORDER BY COUNT(piatto) DESC LIMIT 1); -- using a subquery
 /* ingredient most present in the dish */
-SELECT ingrediente,COUNT(piatto) AS conteggio FROM piatto-menu GROUP BY ingrediente ORDER BY conteggio DESC LIMIT 1;
+SELECT ingrediente,COUNT(piatto) AS conteggio FROM piatto_menu GROUP BY ingrediente ORDER BY conteggio DESC LIMIT 1;
